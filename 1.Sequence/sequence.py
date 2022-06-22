ed335f8ade50012b6d8507001d0ac4e47d72f813
Fatih <dulgerfatih@gmail.com>
2022-06-22 07:12:26 +0100

    Add files via upload

---
+++ 1.Sequence/sequence.py
@@ -0,0 +1,50 @@
+# use the hashtag to comment a line/s of code
+
+# Use the hashtag to comment a line/s of code
+
+"This is a comment"
+"""
+
+This is use for multi line comments
+"""
+
+
+
+# Sequence in programming is a set of instructions performed/executed in order
+
+"""
+
+Execute instruction 1...
+Execute instruction 2...
+Execute instruction 3...
+Execute instruction 4...
+
+"""
+
+# USe the print stateament to output/display name or text or sentence, characters or perform a calculation
+
+print("Fatih") # an instruction to print the string in the speech mark/quotations
+print("c10, welcome to Python programming")
+print(2 + 2)
+
+#\n = newline 
+print("My name is Fatih\nI am in cohort 10.\nI am learning to program in python")
+
+#format output using f-strings
+
+print(f"The answer to {2} + {2} = {2+2}")
+print("The answer to 2 x 5 ", 2 * 5)
+
+# Exercise1 -
+# Write code to perform subtraction
+# Between two numbers
+# Code must be written in one line
+
+# Exercise2: use the print statement to print in new line
+# Your name
+# Your address
+# Your interests
+
+print(f"The answer to {5} - {3} = {5-3}")
+
+print("My name:Fatih\nAddress: 123 Burlington Way, AC5 3DE\nMy interests: programming, gardening, swimming, reading")
