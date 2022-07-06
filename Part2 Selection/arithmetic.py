ddf9871aa78184f923864d018a1faa6a9a00c17c
Fatih <dulgerfatih@gmail.com>
2022-07-06 09:25:30 +0100

    Add files via upload

---
+++ Part2 Selection/arithmetic.py
@@ -0,0 +1,13 @@
+# Arithmetic expression evaluates to a number
+
+num1 = 10
+num2 = 5
+num3 = 11
+num4 = 2
+
+# + operator : plus
+
+print(f"Addition: {num1} + {num2} = {num1 + num2}")
+print(f"Division: {num3} / {num4} = {num3 / num4}") #division
+print(f"Floor Division: {num3} // {num4} = {num3 // num4}") # floor division
+print(f"Modulus/Mod: {num3} Mod {num4} = {num3 % num4}")  # modulus/mod %
