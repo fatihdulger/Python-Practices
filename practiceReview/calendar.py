0b4958f042a12b5b4af2f09564670d1cf19ba0d6
Fatih <dulgerfatih@gmail.com>
2022-08-03 18:40:43 +0100

    Add files via upload

---
+++ practiceReview/calendar.py
@@ -0,0 +1,10 @@
+
+import calendar
+
+print(calendar.calendar(2022, w=2, l=1, c=2, m=6))
+
+#print(calendar.month(theyear = 2021, themonth = 9, w = 4, l = 2))
+
+print("hello world")
+
+
---
+++ practiceReview/cardShuffling.py
@@ -0,0 +1,84 @@
+from random import shuffle
+
+mySuits = ["♥ ", "♦", "♣", "♠"]
+myDeck = []  # empty list
+
+for item in mySuits:
+    for card in range(1, 14):  # start value is 1 end value is 14
+        if card == 11:
+            # change/replace the value of 11 and add it the myDeck list with values ["♥ ","♦", "♣", "♠"]
+            myDeck.append("J" + item)
+        elif card == 12:
+            myDeck.append("Q" + item)
+        elif card == 13:
+            myDeck.append("K" + item)
+        elif card == 1:
+            myDeck.append("A" + item)
+        else:
+            myDeck.append(str(card) + item)
+print(f"Unshuffled deck of cards\n {myDeck}")
+shuffle(myDeck)
+print(f"Shuffled deck of cards\n {myDeck}")
+
+
+
+mySuits = ["♥", "♦", "♣", "♠"]
+
+myDeck = []  # empty list
+
+
+
+for item in mySuits:
+
+    for card in range(1, 14):  # start value is 1 end value is 14
+
+        # if card == 11:
+
+        #     # change/replace the value of 11 and add it the myDeck list with values ["♥ ","♦", "♣", "♠"]
+
+        #     myDeck.append("J" + item)
+
+        # elif card == 12:
+
+        #     myDeck.append("Q" + item)
+
+        # elif card == 13:
+
+        #     myDeck.append("K" + item)
+
+        # elif card == 1:
+
+        #     myDeck.append("A" + item)
+
+        # else:
+
+        myDeck.append(str(card) + item)
+
+       
+
+           
+
+print(f"Unshuffled deck of cards\n {myDeck}")
+
+
+
+# shuffle(myDeck)
+
+# print(f"Shuffled deck of cards\n {myDeck}")
+
+
+list1 = [1, 2, 3, 4, 5, 6, 7, 8, 9]
+shuffled = shuffle(list1)
+print(list1)
+
+
+
+
+
+mylist = ["apple", "banana", "cherry"]
+
+shuffle(mylist)
+
+
+
+print(mylist)
